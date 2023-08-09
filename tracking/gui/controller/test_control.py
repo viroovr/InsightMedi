d0a3cd4aedb5074a6cf35a78a728a9c1018ac799
viroovr <tjgusdnjs789@gmail.com>
2023-08-09 17:55:17 +0900

    Update multi tracker
    use multi tracker api
    	- legacy multi tracker is used

--- tracking/gui/controller/test_control.py
+++ tracking/gui/controller/test_control.py
@@ -36,6 +36,7 @@ class Controller():
         self.is_move = False
 
         self.press = None
+        
         self.tracker = {}
         self.is_tracking = False
 
@@ -60,7 +61,6 @@ class Controller():
         self.start = None
         self.end = None
         self.is_drawing = False
-
         self.selector_mode = "drawing"
         self.annotation_mode = mode
         print(f"Drawing mode : {mode}")
@@ -302,7 +302,7 @@ class Controller():
             patch.remove()
         for patch in self.ax.lines:
             patch.remove()
-        self.annotation = []
+        self.annotation.clear()
         self.canvas.draw()
 
     # modify functions
@@ -344,6 +344,7 @@ class Controller():
             self.set_edge_thick(patch)
         self.canvas.draw()
         self.current_annotation = None
+        self.is_tracking = False
         self.annotation = []
 
     def label_clicked(self, frame, _label_name=[]):
@@ -355,11 +356,11 @@ class Controller():
                                         empty list일 경우, self.annotation 라벨들의 강조를 유지합니다.
         """
         if not _label_name and self.annotation:
-            _label_name = [an.get_label() for an in self.annotation]
-        # print(f"{sys._getframe(0).f_code.co_name}: {_label_name}")
+            _label_name = (an.get_label() for an in self.annotation)
         self.erase_all_annotation()
 
         frame_directory = self.dd.frame_label_dict[frame]
+        print(f"frame_directory : {frame_directory}")
         for drawing_type in frame_directory:
             label_directory = frame_directory[drawing_type]
             for label in label_directory:
@@ -375,7 +376,8 @@ class Controller():
                 if label in _label_name:
                     # print(f"label : {label}, _label_name : {_label_name}")
                     self.select_current_edge(annotation)
-
+        # print(f"{sys._getframe(0).f_code.co_name}: {self.annotation}")
+        
         self.canvas.draw()
 
     def img_show(self, img, cmap='viridis', init=False, clear=False):
@@ -398,13 +400,13 @@ class Controller():
 
     #object tracking
     def init_object_tracking(self):
+        
         bbox = self.check_bbox()   # object tracking이 가능한 상태인 지 확인하는 함수
         frame = self.dd.image
         if bbox:
             self.gui.slider.setValue(self.dd.frame_number + 1)   # 다음 frame으로 업데이트
             if not self.is_tracking:
                 # object tracking 한 결과 나온 라벨링 그리기
-                self.tracker = {}
                 self.object_tracking(frame, bbox, init=True)
                 self.is_tracking = True
             else:
@@ -420,19 +422,19 @@ class Controller():
         """
         label_list = self.dd.frame_label_check(self.dd.frame_number)
         next_label_list = self.dd.frame_label_check(self.dd.frame_number + 1)
-        bbox = {}
+        bbox = []
         if self.dd.file_mode == 'mp4' and label_list and self.annotation:
             for annotation in self.annotation:
                 label = annotation.get_label()
                 if label not in next_label_list:
                     coord_list = self.dd.frame_label_dict[self.dd.frame_number]['rectangle'][label]['coords']
+                    color = self.dd.frame_label_dict[self.dd.frame_number]['rectangle'][label]['color']
                     x = coord_list[0][0]
                     y = coord_list[0][1]
                     w = coord_list[1]
                     h = coord_list[2]
-                    label_bbox = [int(x), int(y), int(w), int(h)]
-                    bbox[label] = label_bbox
-                    print(f"{label}의 bbox:", label_bbox)
+                    bbox.append((label, [int(x), int(y), int(w), int(h)], color))
+                    # print(f"{label}의 bbox:", bbox)
             print("현재 프레임의 bbox들 좌표:", bbox)
         return bbox
 
@@ -444,31 +446,44 @@ class Controller():
             bbox (List): bounding box의 좌표 리스트
             init (bool, optional): True일때, tracker를 생성합니다. Defaults to False.
         """
-        for label, label_bbox in bbox.items():
-            if init:
-                print(f"{label}의 tracker 초기화")
-                self.tracker[label] = cv2.TrackerCSRT_create()
-                ok = self.tracker[label].init(frame, label_bbox)
+        
+        if init:
+            print(f"multitracker 초기화")
+            self.multitracker = cv2.legacy.MultiTracker_create()
+            # ok = self.tracker[label].init(frame, label_bbox)
+            for label_bbox in bbox:
+                tracker = cv2.legacy.TrackerCSRT_create()
+                self.multitracker.add(tracker, frame, label_bbox[1])
 
-            ok, new_bbox = self.tracker[label].update(frame)
+            
+        ok, new_bboxes = self.multitracker.update(frame)
 
-            if ok:
-                # print(f"obect tracking한 {label}의 bbox", new_bbox)
-
-                # 새로운 라벨 저장을 위해 필요한 데이터들
-                bbox_ = ((new_bbox[0], new_bbox[1]), new_bbox[2], new_bbox[3])
-                print(self.dd.frame_label_check(self.dd.frame_number - 1))
-                # label = self.annotation[0].get_label()
-                color = self.dd.frame_label_dict[self.dd.frame_number -
-                                                 1]['rectangle'][label]['color']
-
-                # 라벨 그리기 및 저장
-                self.annotation.pop(0)
+        if ok:
+            print(f"기존 bbox {bbox}" )
+            print(f"object tracking한 bbox {new_bboxes}")
+            print(f"선택된 annotation {self.annotation}")
+            for i, new_bbox in enumerate(new_bboxes):
+            # 새로운 라벨 저장을 위해 필요한 데이터들
+            # bbox_ = ((new_bbox[0], new_bbox[1]), new_bbox[2], new_bbox[3])
+                bbox_ = ((int(new_bbox[0]), int(new_bbox[1])), int(new_bbox[2]), int(new_bbox[3]))
+            # print(self.dd.frame_label_check(self.dd.frame_number - 1))
+            # label = self.annotation[0].get_label()
+                color = bbox[i][2]
+                label = bbox[i][0]
+                print(*bbox_, color, label)
+            # 라벨 그리기 및 저장
+                if self.annotation:
+                    self.annotation.pop(0)
                 new_annotation = self.ax.add_patch(
-                    Rectangle((new_bbox[0], new_bbox[1]), new_bbox[2], new_bbox[3],
-                              fill=False, picker=True, label=label, edgecolor=color))
+                    Rectangle(*bbox_,
+                            fill=False, picker=True, label=label, edgecolor=color))
                 self.select_current_edge(new_annotation)
                 # print("현재 선택된 label들:", self.annotation)
                 self.dd.add_label('rectangle', label, bbox_, color,
-                                  frame_number=self.dd.frame_number)
+                                frame_number=self.dd.frame_number)
                 # print(self.dd.frame_label_dict)
+        else:
+            print("object tracking failed")
+    
+    def label_move(self):
+        pass
